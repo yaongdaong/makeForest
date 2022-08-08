948243aef0ddb37e9ae1e2683dd9024d93cb5407
yaongdaong <haeryang2@naver.com>
2022-08-09 00:16:28 +0900

    first

--- src/main/java/com/study/board/service/BoardService.java
+++ src/main/java/com/study/board/service/BoardService.java
@@ -8,9 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-
 import java.io.File;
-import java.util.List;
 import java.util.UUID;
 
 @Service
